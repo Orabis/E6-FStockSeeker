fb28998e32532dd130543e4056d69ef5ca8f6e16
Léo <cdf.leo.merkel@gmail.com>
2025-01-08 15:53:33 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+
+<script setup>
+import Button from "primevue/button"
+import InputText from 'primevue/inputtext';
+import HelloWorld from "./components/HelloWorld.vue";
+
+</script>
+
+<template>
+  <span>Test PrimeVue</span>
+  <div>
+    <Button label="Save" />
+    <InputText type="text" v-model="value" />
+  </div>
+  <HelloWorld>
+  </HelloWorld>
+</template>
